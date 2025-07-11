e5045f417b3fc631fc9b970661f30eb19d9c720b
usernamedinesh <borod9200@gmail.com>
2025-07-11 22:45:15 +0530

    initialisation of bhvr project

---
+++ client/src/App.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import beaver from './assets/beaver.svg'
+import { hcWithType } from 'server/dist/client'
+
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
+
+type ResponseType = Awaited<ReturnType<typeof client.hello.$get>>;
+
+const client = hcWithType(SERVER_URL);
+
+function App() {
+  const [data, setData] = useState<Awaited<ReturnType<ResponseType["json"]>> | undefined>()
+
+  async function sendRequest() {
+    try {
+      const res = await client.hello.$get()
+      if (!res.ok) {
+        console.log("Error fetching data")
+        return
+      }
+      const data = await res.json()
+      setData(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="max-w-xl mx-auto flex flex-col gap-6 items-center justify-center min-h-screen">
+      <a href="https://github.com/stevedylandev/bhvr" target="_blank">
+        <img
+          src={beaver}
+          className="w-16 h-16 cursor-pointer"
+          alt="beaver logo"
+        />
+      </a>
+      <h1 className="text-5xl font-black">bhvr</h1>
+      <h2 className="text-2xl font-bold">Bun + Hono + Vite + React</h2>
+      <p>A typesafe fullstack monorepo</p>
+      <div className='flex items-center gap-4'>
+        <button
+          onClick={sendRequest}
+          className="bg-black text-white px-2.5 py-1.5 rounded-md"
+        >
+          Call API
+        </button>
+        <a target='_blank' href="https://bhvr.dev" className='border-1 border-black text-black px-2.5 py-1.5 rounded-md'>
+          Docs
+        </a>
+      </div>
+        {data && (
+          <pre className="bg-gray-100 p-4 rounded-md">
+            <code>
+            Message: {data.message} <br />
+            Success: {data.success.toString()}
+            </code>
+          </pre>
+        )}
+    </div>
+  )
+}
+
+export default App
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+Next steps:
+  bun run dev:client   # Start the client
+  bun run dev:server   # Start the server in another terminal
+  bun run dev          # Start all

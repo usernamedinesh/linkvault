f08702a380da53d0c645c9ec83e4891b8980b71a
usernamedinesh <borod9200@gmail.com>
2025-07-12 18:02:16 +0530

    working sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -4,9 +4,9 @@ function Sidebar() {
     return(
         <> 
             <nav className = "h-[100vh] w-[18vw] bg-black font-white text-white ">
-                <NavLink to="/login"> Login </ NavLink>
-                <NavLink to="/signup"> Signup </ NavLink>
-                <NavLink to="/signup"> Logout </ NavLink>
+                <NavLink to="/login" className=" border-2 rounded-sm py-3 flex px-5"> Login </ NavLink>
+                <NavLink to="/signup" className="border-2 rounded-sm py-3 flex px-5"> Signup </ NavLink>
+                <NavLink to="/signup" className="border-2 rounded-sm py-3 flex px-5"> Logout </ NavLink>
             </nav>
         </>
     )

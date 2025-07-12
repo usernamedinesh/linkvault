65cb41d9d1ef9daed3c89d5c3b17935aa8995e3b
usernamedinesh <borod9200@gmail.com>
2025-07-12 16:57:21 +0530

    added sidebar with mousemove

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,32 @@
+//how to show sidebar here 
+import Sidebar from "../components/Sidebar";
+import {useState} from "react";
+
 function Home() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const toggleSidebar = () => setIsSidebarOpen(prev => !prev);
+    const clicked = () => {
+        return <Sidebar />
+    }
+    const handleMouseMove = (e) => {
+        if (e.clientX <= 1){
+            setIsSidebarOpen(true);
+        } 
+    }
+    const handleMouseLeave = () => {
+        setIsSidebarOpen(false);
+    }
+
     return (
-        <div>
-            <h1>Home page </h1>
+        <div onMouseMove = {handleMouseMove} style = {{height:"100vh"}} className="flex gap-30">
+            <div onMouseLeave = { handleMouseLeave} >
+                { isSidebarOpen && <Sidebar /> }
+            </div>
+            <div>
+                <h1>Home page </h1>
+                <h1>need to show sidebar here  </h1>
+
+            </div>
         </div>
     )
 }

401d103738b6902c38f8c33c2c77f105b2ed7f11
usernamedinesh <borod9200@gmail.com>
2025-07-14 20:12:09 +0530

    add zod type for newpassword and forgotpassword

--- shared/src/types/auth.ts
+++ shared/src/types/auth.ts
@@ -17,3 +17,17 @@ export const signupSchema = z.object ({
 })
 
 export type Signup = z.infer<typeof signupSchema>; 
+
+//Forgot-password Schema
+export const forgotPasswordSchema = z.object ({
+    email: z.string().email(),
+})
+
+export type ForgotPassword = z.infer<typeof forgotPasswordSchema>; 
+
+//new password schema
+export const newPasswordSchema = z.object ({
+    password: z.string().min(4).max(30),
+    confirmPassword: z.string().min(4).max(30),
+})
+export type NewPassword = z.infer<typeof newPasswordSchema>; 

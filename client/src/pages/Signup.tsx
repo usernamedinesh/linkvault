54a03a88677982385096cb08b0653c45c72dbcfa
usernamedinesh <borod9200@gmail.com>
2025-07-14 20:02:37 +0530

    add zod validation in singup

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,14 +1,29 @@
 import { useState } from "react"
+import {signupSchema, type Signup} from "shared";
+import { zodResolver } from "@hookform/resolvers/zod"; 
 
 function Signup(){
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+    const [errors, setErrors] = useState<{ email?: string; password?: string; name?: string }>({});
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Email:", email);
-        console.log("Password:", password);
-    }
+        const result = signupSchema.safeParse({email, password, name });
+        if (!result.success) {
+            const fieldErrors = result.error.flatten().fieldErrors;
+            setErrors({
+                email: fieldErrors.email?.[0],
+                password: fieldErrors.password?.[0],
+                name: fieldErrors.name?.[0],
+            });
+
+            return;
+        }
+        const data: Signup = result.data; 
+        console.log("Validated singup data: ", data);
+    };
 
     return(
         <>
@@ -20,26 +35,40 @@ function Signup(){
                         type="text"
                         placeholder="👤 Name"
                         value={name}
-                        onChange={(e) => setName(e.target.value)}
+                        onChange={(e) => {setName(e.target.value);
+                            if (errors.name) setErrors((prev) => ({ ...prev, name: undefined}));
+                        }}
+
                         required
                         className="text-black px-4 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-500"
                     />
+
+                     {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
+
                     <input
                         type="email"
                         placeholder="📧 Email"
                         value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        onChange={(e) => { setEmail(e.target.value)
+                         if (errors.email) setErrors((prev) => ({...prev, email: undefined}));   
+                        }}
                         required
                         className="text-black px-4 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-500"
                     />
+
+                     {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
                     <input
                         type="password"
                         placeholder="🔒 Password"
                         value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        onChange={(e) => { setPassword(e.target.value)
+                            if (errors.password) setErrors((prev) => ({...prev, password: undefined}));   
+                        }}
                         required
                         className=" text-black px-4 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-500"
                     />
+
+                     {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
 
                     {/* ✅ Tailwind-styled button */}
                     <button

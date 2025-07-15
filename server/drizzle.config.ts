9b0e5b633d7a1c57caab557fe341d7cfde13640d
usernamedinesh <borod9200@gmail.com>
2025-07-15 20:41:10 +0530

    add secret of db

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -2,9 +2,16 @@ import {Config} from "drizzle-kit";
 import dotenv from "dotenv";
 dotenv.config();
 
+POSTGRES_DB
 export default {
     schema: "./src/db/schema.ts",
     out: "./drizzle/migrations",
     driver: "pg",
-    dbCredentials: { connectionString: process.env.DATABASE_URL! },
+    dbCredentials: {
+        host: "localhost", 
+        port: 5432,
+        user: process.env.POSTGRES_USER!,
+        password: process.env.POSTGRES_PASSWORD!,
+        database: process.env.POSTGRES_DB!,
+    },
 } satisfies Config;

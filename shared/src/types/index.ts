a6abea0439c4ba50bb2e592b99092f4820e40861
usernamedinesh <borod9200@gmail.com>
2025-07-15 22:41:41 +0530

    add ApiError type in shared

--- shared/src/types/index.ts
+++ shared/src/types/index.ts
@@ -3,7 +3,13 @@ export type ApiResponse<T  = undefined >  = {
     success: true;
     status: number;
     data?: T;
-}
+};
+
+export type ApiError = {
+    status: number;
+    message: string;
+    error?: string | Record<string, string>;
+};
 
 //exportable  files are here 
 export * from "./link";

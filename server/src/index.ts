9c5ba5c8cb1fc0f64b88c66fe284d09a9d2cd2aa
usernamedinesh <borod9200@gmail.com>
2025-07-16 11:39:14 +0530

    add middleware for paring req.body

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,9 +5,18 @@ import routes from "./routes";
 
 export const app = new Hono()
 
-.use(cors())
 
-.route("/", healthRouter)
-.route("/api", routes)
+app.use('*', async (c, next) => {
+  if (c.req.header('content-type')?.includes('application/json')) {
+    const body = await c.req.json();
+    c.set('body', body);
+  }
+  await next();
+});
+app.use(cors())
+
+
+app.route("/", healthRouter)
+app.route("/api", routes)
 
 export default app;

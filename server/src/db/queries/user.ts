25d974d295d852181d05da47121483dd11743f78
usernamedinesh <borod9200@gmail.com>
2025-07-16 10:41:16 +0530

    add auth route in entry ponint

--- server/src/db/queries/user.ts
+++ server/src/db/queries/user.ts
@@ -1,17 +1,27 @@
 import { eq } from "drizzle-orm";
 import { dbPromise } from "../index";
-import {users} from  from "../schema";
+import { users } from "../schema";
 
 export async function findUserByEmail(email: string) {
-    const result = await db.select().from(users).where(eq(users.email, email)).limit(1);
-    return result[0];
+  const db = await dbPromise; // Await the DB instance
+  const result = await db.select().from(users).where(eq(users.email, email)).limit(1);
+  return result[0] || null;
 }
 
-export async function createUser({name, email, password }: {name:string; email: string; password: string}) {
-    const result = await db
-        .insert(users)
-        .values({name, email, password})
-        .returning();
-    return result[0];
-
+export async function createUser({
+  name,
+  email,
+  password,
+}: {
+  name: string;
+  email: string;
+  password: string;
+}) {
+  const db = await dbPromise;
+  const result = await db
+    .insert(users)
+    .values({ name, email, password })
+    .returning();
+  return result[0];
 }
+
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import type { ApiResponse } from "shared/dist";
 import { healthRouter } from "./routes/health";
+import routes from "./routes";
 
 export const app = new Hono()
 
 .use(cors())
 
 .route("/", healthRouter)
+.route("/api", routes)
 
 export default app;

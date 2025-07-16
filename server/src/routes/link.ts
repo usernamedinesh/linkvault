c5d248a36bbe8e7ba76f31f7fd5cd54c440d4975
usernamedinesh <borod9200@gmail.com>
2025-07-16 17:54:02 +0530

    auth to link in auth

--- server/src/routes/link.ts
+++ server/src/routes/link.ts
@@ -2,11 +2,11 @@
 import { Hono } from "hono"
 import { create_link, get_link, update_link, delete_link } from "../controllers/link";
 
- const link = new Hono();
+const link = new Hono();
 
-auth.post("/create-link", create_link);
-auth.get("/get-link", get_link);
-auth.delete("/delete-link", delete_link);
-auth.put("/update-link", update_link);
+link.post("/create-link", create_link);
+link.get("/get-link", get_link);
+link.delete("/delete-link", delete_link);
+link.put("/update-link", update_link);
 
 export default link;
--- shared/src/types/link.ts
+++ shared/src/types/link.ts
@@ -2,7 +2,7 @@ import  { z } from "zod";
 
 //Link Schema
 export const LinkSchema = z.object ({
-    id: z.string(),
+    // id: z.string(),
     url: z.string().url(),
     title: z.string(),
     tags: z.array(z.string()),

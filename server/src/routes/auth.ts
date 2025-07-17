9437afd876a34e975cd6d07bc4cb009e1b57bdfe
usernamedinesh <borod9200@gmail.com>
2025-07-17 17:00:21 +0530

    add profile route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono"
-import { signup, login, forgot_password, new_password } from "../controllers/auth";
+import { signup, login, forgot_password, new_password, get_profile } from "../controllers/auth";
 
  const auth = new Hono();
 
@@ -7,5 +7,6 @@ auth.post("/signup", signup);
 auth.post("/login", login);
 auth.post("/forgot-password", forgot_password);
 auth.post("/new-password", new_password);
+auth.get("/profile/:userId", get_profile);
 
 export default auth;
